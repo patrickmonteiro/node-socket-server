e7b8688351709b5ff114531a12c277929918484c
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2019-08-15 00:02:54 -0300

    ok

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3002;
 
-app.get('/', function(req, res){
+app.get('/*', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
